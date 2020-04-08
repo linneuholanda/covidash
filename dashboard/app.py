44a541fc2d7228ca256c294187a5e78dcdeb39cd
linneuholanda <linneuholanda@gmail.com>
2020-04-08 17:37:10 -0300

    Plots starting from 100 cases

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,12 +3,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-from epimodels.continuous.models import SEQIAHR
+#from epimodels.continuous.models import SEQIAHR
 st.title('Cenarios de Controle da Covid-19')
 
 
 WHOLE_BRASIL = "Brasil inteiro"
-PAGE_CASE_NUMBER = "Evolução do Número de Casos"
+
+### Main menu goes here
+HOME = "Home"
+MODELS = "Modelos"
+DATA = "Dados"
+CASE_TIME_SERIES = "Evolução do Número de Casos"
+CUM_CASE_COUNT = "Casos acumulados a partir de 100"
 
 COLUMNS = {
     "A": "Assintomáticos",
@@ -33,11 +39,11 @@ logo = Image.open('dashboard/logo_peq.png')
 
 def main():
     st.sidebar.image(logo, use_column_width=True)
-    page = st.sidebar.selectbox("Escolha um Painel", ["Home",  "Modelos", "Dados", PAGE_CASE_NUMBER])
-    if page == "Home":
+    page = st.sidebar.selectbox("Escolha um Painel", [HOME,MODELS,DATA,CASE_TIME_SERIES, CUM_CASE_COUNT])
+    if page == HOME:
         st.header("Dashboard COVID-19")
         st.write("Escolha um painel à esquerda")
-    elif page == 'Modelos':
+    elif page == MODELS:
         st.title("Explore a dinâmica da COVID-19")
         st.sidebar.markdown("### Parâmetros do modelo")
 
@@ -80,13 +86,13 @@ def main():
         )
         plot_model(melted_traces, q)
 
-    elif page == "Dados":
+    elif page == DATA:
         st.title('Probabilidade de Epidemia por Município')
         probmap = Image.open('dashboard/Outbreak_probability_full_mun_2020-04-06.png')
         st.image(probmap, caption='Probabilidade de Epidemia em 6 de abril',
         use_column_width=True)
 
-    elif page == PAGE_CASE_NUMBER:
+    elif page == CASE_TIME_SERIES:
         st.title("Casos Confirmados no Brasil")
         data = get_data()
         ufs = sorted(list(data.state.drop_duplicates().values))
@@ -98,10 +104,31 @@ def main():
             city_options = st.multiselect("Selecione os Municípios", cities)
 
         is_log = st.checkbox('Escala Logarítmica', value=False)
+        #is_aligned = st.checkbox("Alinhar por primeiros 100 casos",value=False)
         data_uf = get_data_uf(data, uf_option, city_options)
+        #data_uf = data_uf[data_uf>=100] if is_aligned else data_uf
         data_uf = np.log(data_uf + 1) if is_log else data_uf
-
         st.line_chart(data_uf, height=400)
+        
+    elif page==CUM_CASE_COUNT:
+        st.title("Casos acumulados a partir do centésimo")
+        data = get_data()
+        ufs = sorted(list(data.state.drop_duplicates().values))
+        uf_option = st.multiselect("Selecione o Estado", ufs)
+
+        city_options = None
+        if uf_option:
+            cities = get_city_list(data, uf_option)
+            city_options = st.multiselect("Selecione os Municípios", cities)
+        is_log = st.checkbox('Escala Logarítmica', value=False)
+        #is_aligned = st.checkbox("Alinhar por primeiros 100 casos",value=False)
+        data_uf = get_data_uf(data, uf_option, city_options)
+        #print(data_uf)
+        data_uf = get_aligned_data(data_uf,align=100)
+        #data_uf = data_uf[data_uf>=100] if is_aligned else data_uf
+        data_uf = np.log(data_uf + 1) if is_log else data_uf
+        st.line_chart(data_uf, height=400)
+     #elif page
 
 
 def plot_model(melted_traces, q):
@@ -122,6 +149,20 @@ def plot_model(melted_traces, q):
         a="%d" % q
     )
     st.altair_chart(la)
+    
+def plot_aligned(df,align=100,log=False):
+    pass
+    #fig,ax = subplots(1,1,figsize=(15,8))
+    #alinhados = 
+    #alinhados = pd.concat([suecia[suecia>100].reset_index(),alemanha[alemanha>100].reset_index(),
+    #       espanha[espanha>100].reset_index(),
+    #       italia[italia>100].reset_index()], 
+    #      axis=1)
+    #alinhados['EUA'] = serie_US[serie_US.US>100].reset_index().US
+    #alinhados['Brasil'] = df_brasil[df_brasil.casosAcumulados>100].reset_index().casosAcumulados
+    #alinhados.plot(ax=ax,logy=True, grid=True);
+    #plt.savefig('export/Brasil_vs_outros.png', dpi=300)
+    #alinhados.to_csv('export/Brasil_vs_outros.csv')
 
 
 @st.cache(suppress_st_warning=True)
@@ -157,10 +198,30 @@ def get_data_uf(data, uf, city_options):
             data = pd.DataFrame(pivot_data.to_records())
 
     else:
-        return data.loc[data.place_type == "city"].groupby("date")["Casos Confirmados"].sum()
+        return data.loc[data.place_type == "city"].groupby("date")["Casos Confirmados"].sum().to_frame()
 
     return data.set_index("date")
 
+def get_aligned_data(df,align=100):
+    align_dfs = [df.loc[df[c]>=100,[c]].values.reshape(-1,) for c in df.columns] 
+    columns = [c for c in df.columns] 
+    aligned_df = pd.DataFrame(align_dfs,index=columns).T                           
+    #align_dfs = [d.reset_index() for d in align_dfs]
+    #aligned = pd.concat([d for d in align_dfs],ignore_index=True)
+    return aligned_df
+    #def plot_aligned(df,align=100,log=False):
+    #pass
+    #fig,ax = subplots(1,1,figsize=(15,8))
+    #alinhados = 
+    #alinhados = pd.concat([suecia[suecia>100].reset_index(),alemanha[alemanha>100].reset_index(),
+    #       espanha[espanha>100].reset_index(),
+    #       italia[italia>100].reset_index()], 
+    #      axis=1)
+    #alinhados['EUA'] = serie_US[serie_US.US>100].reset_index().US
+    #alinhados['Brasil'] = df_brasil[df_brasil.casosAcumulados>100].reset_index().casosAcumulados
+    #alinhados.plot(ax=ax,logy=True, grid=True);
+    #plt.savefig('export/Brasil_vs_outros.png', dpi=300)
+    #alinhados.to_csv('export/Brasil_vs_outros.csv')
 
 @st.cache
 def get_city_list(data, uf):
